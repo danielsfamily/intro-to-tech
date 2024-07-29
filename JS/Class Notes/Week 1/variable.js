6b985594abe6d480a61efd093892d8f361f1b1bd
danielsfamiy <danielsfamilyks@gmail.com>
2024-07-29 09:15:37 -0500

    class notes commit

--- JS/Class Notes/Week 1/variable.js
+++ JS/Class Notes/Week 1/variable.js
@@ -1,4 +1,4 @@
-//variables are containers that store vcalues to be recalled later and we use three specific words to declaere let,const,var
+//variables are containers that store values to be recalled later and we use three specific words to declare let,const,var
 // you can not use numbers at the beginning of the variable
 //JavaScript variable name does not allow specific signs except $ and _. Variables use camel case. VBariable name should not have a space between the words
 //string
